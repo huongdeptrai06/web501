d4c7aa42985e7fca9022e2f0452be2dd1ee9daa9
Tran Phu Huong <tranphuhuong1802@gmail.com>
2025-11-14 14:48:47 +0700

    ok

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,19 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-    const greeting = "Chào mừng đến với React!";
-    const name = "Trần Phú Hương";
-    const age = 20;
+function Profile() {
+    const name = "Đẹp Trai";
+    const studentId = "SV001";
+    const major = "Công nghệ Thông tin";
+
     return (
-      
-        <div className="container">
-            <h1>{greeting}</h1>
-            <p>Đây là component đầu tiên của bạn</p>
-            <h1>Xin chào {name}</h1>
-            <p>Tuổi: {age + 1}</p>
-            <p>Là học viên: {true ? "Có" : "Không"}</p>
-        </div> 
+        <div className="profile-card">
+            <h2>Thông tin sinh viên</h2>
+            <p>
+                <strong>Họ tên:</strong> {name}
+            </p>
+            <p>
+                <strong>Mã SV:</strong> {studentId}
+            </p>
+            <p>
+                <strong>Ngành học:</strong> {major}
+            </p>
+        </div>
     );
 }
-export default App
+export default Profile;
