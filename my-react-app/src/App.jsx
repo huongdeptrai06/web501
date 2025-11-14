39f235681f93637355d58929571a4a92f6e2867b
Tran Phu Huong <tranphuhuong1802@gmail.com>
2025-11-14 14:27:54 +0700

    ok

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,12 +5,17 @@ import './App.css'
 
 function App() {
     const greeting = "Chào mừng đến với React!";
-
+    const name = "Trần Phú Hương";
+    const age = 20;
     return (
+      
         <div className="container">
             <h1>{greeting}</h1>
             <p>Đây là component đầu tiên của bạn</p>
-        </div>
+            <h1>Xin chào {name}</h1>
+            <p>Tuổi: {age + 1}</p>
+            <p>Là học viên: {true ? "Có" : "Không"}</p>
+        </div> 
     );
 }
 export default App
