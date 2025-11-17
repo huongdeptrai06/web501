c5141069624a76851cb8c5334461c85cb996f711
Tran Phu Huong <tranphuhuong1802@gmail.com>
2025-11-17 15:29:55 +0700

    ok

--- my-react-app/src/components/Heading.jsx
+++ my-react-app/src/components/Heading.jsx
@@ -1,8 +1,9 @@
 export default function Heading({ title = 'Tour Nội Địa' }) {
   return (
-    <h1 className="font-bold uppercase text-blue-600 border-b-2 border-blue-600 pb-1">
+    <h1 className="text-3xl font-bold uppercase text-blue-600 border-b-2 border-orange-400 pb-2 text-center">
       {title}
     </h1>
   )
 }
+
 
